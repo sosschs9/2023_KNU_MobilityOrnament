5d545f8fa6aa893a5be9a803dcc8d53b1fd24839
송혜경 <sosschs9@naver.com>
2023-01-17 02:10:00 +0900

    RC_Car Serial Monitoring

---
+++ RC_Car_DB/RC_Car_DB/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace RC_Car_DB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title3 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.num = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.date = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.sensor = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.distance = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.deltha = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 25F);
+            this.label1.Location = new System.Drawing.Point(14, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "COM포트";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("굴림", 20F);
+            this.textBox1.Location = new System.Drawing.Point(205, 19);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(154, 38);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("굴림", 15F);
+            this.button1.Location = new System.Drawing.Point(366, 19);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 48);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "연결";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("굴림", 15F);
+            this.button2.Location = new System.Drawing.Point(528, 19);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 48);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "종료";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM4";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 20F);
+            this.label2.Location = new System.Drawing.Point(27, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 27);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "sensor : -";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("굴림", 20F);
+            this.label3.Location = new System.Drawing.Point(253, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(162, 27);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "distance : -";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("굴림", 15F);
+            this.button3.Location = new System.Drawing.Point(32, 500);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(155, 48);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "불러오기";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(577, 149);
+            this.chart1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.chart1.Name = "chart1";
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series7.Legend = "Legend1";
+            series7.Name = "collision";
+            series8.ChartArea = "ChartArea1";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series8.Legend = "Legend1";
+            series8.Name = "distance";
+            series9.ChartArea = "ChartArea1";
+            series9.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series9.Legend = "Legend1";
+            series9.Name = "deltha";
+            this.chart1.Series.Add(series7);
+            this.chart1.Series.Add(series8);
+            this.chart1.Series.Add(series9);
+            this.chart1.Size = new System.Drawing.Size(496, 341);
+            this.chart1.TabIndex = 8;
+            this.chart1.Text = "chart1";
+            title3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            title3.Name = "Title1";
+            title3.Text = "RC_car";
+            this.chart1.Titles.Add(title3);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("굴림", 15F);
+            this.button4.Location = new System.Drawing.Point(205, 500);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(155, 48);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "저장하기";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("굴림", 20F);
+            this.label4.Location = new System.Drawing.Point(509, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 27);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "deltha : -";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.num,
+            this.date,
+            this.sensor,
+            this.distance,
+            this.deltha});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(32, 149);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(503, 340);
+            this.listView1.TabIndex = 11;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // num
+            // 
+            this.num.Text = "num";
+            // 
+            // date
+            // 
+            this.date.Text = "date";
+            this.date.Width = 212;
+            // 
+            // sensor
+            // 
+            this.sensor.Text = "sensor";
+            // 
+            // distance
+            // 
+            this.distance.Text = "distance";
+            this.distance.Width = 74;
+            // 
+            // deltha
+            // 
+            this.deltha.Text = "deltha";
+            this.deltha.Width = 63;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1105, 569);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("굴림", 11F);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader num;
+        private System.Windows.Forms.ColumnHeader date;
+        private System.Windows.Forms.ColumnHeader sensor;
+        private System.Windows.Forms.ColumnHeader distance;
+        private System.Windows.Forms.ColumnHeader deltha;
+    }
+}
+
---
+++ RC_Car_DB/RC_Car_DB/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace RC_Car_DB
+{
+    public partial class Form1 : Form
+    {
+        string Conn = "Server=localhost;port=3310;Database=rc_car_db;Uid=root;Pwd=1234;Allow User Variables=True;";
+        string start, end, user = "홍길동";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("입력하세요!");
+            }
+            else
+            {
+                if (serialPort1.IsOpen == false)
+                {
+                    serialPort1.PortName = textBox1.Text;
+                    serialPort1.Open();
+                    // DB 삭제
+                    using (MySqlConnection conn = new MySqlConnection(Conn))
+                    {
+                        conn.Open();
+                        MySqlCommand msc = new MySqlCommand("delete from drive_data", conn);
+                        msc.ExecuteNonQuery();
+
+                        MySqlCommand msc2 = new MySqlCommand("alter table drive_data auto_increment=0;", conn);
+                        msc2.ExecuteNonQuery();
+                    }
+                    // 시작시간, username
+                    start = DateTime.Now.ToString();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen == true)
+            {
+                // 종료시간
+                end = DateTime.Now.ToString();
+
+                // 총 대여시간
+                DateTime StartDate = Convert.ToDateTime(start);
+                DateTime EndDate = Convert.ToDateTime(end);
+
+                TimeSpan dateDiff = EndDate - StartDate;
+
+                int diffDay = dateDiff.Days;
+                int diffHour = dateDiff.Hours;
+                int diffMinute = dateDiff.Minutes;
+                int diffSecond = dateDiff.Seconds;
+
+                int rental_time = (int)(diffDay * 86400 + diffHour * 3600 + diffMinute * 60 + diffSecond);
+
+                //DB에 데이터 삽입
+                using (MySqlConnection conn = new MySqlConnection(Conn))
+                {
+                    conn.Open();
+                    MySqlCommand msc = new MySqlCommand("insert into car_data(user, start, end, rental_time) values('" + user + "','" + start + "','" + end + "', " + rental_time + ")", conn);
+                    msc.ExecuteNonQuery();
+                }
+
+                serialPort1.Close();   
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            string rawdata = serialPort1.ReadLine();
+            string[] data = rawdata.Split('/');
+
+
+            if (label2.InvokeRequired)
+            {
+                label2.Invoke(new MethodInvoker(delegate { label2.Text = "sensor : " + data[0]; }));
+            }
+            else
+                label2.Text = "sensor : " + data[0];
+
+            if (label3.InvokeRequired)
+            {
+                label3.Invoke(new MethodInvoker(delegate { label3.Text = "distance : " + data[1]; }));
+            }
+            else
+                label3.Text = "distance : " + data[1];
+
+            if (label4.InvokeRequired)
+            {
+                label4.Invoke(new MethodInvoker(delegate { label4.Text = "deltha : " + data[2]; }));
+            }
+            else
+                label4.Text = "deltha : " + data[2];
+
+
+            string date = DateTime.Now.ToString();
+
+            //DB에 데이터 삽입
+            using (MySqlConnection conn = new MySqlConnection(Conn))
+            {
+                conn.Open();
+                MySqlCommand msc = new MySqlCommand("insert into drive_data(date,collision,distance,deltha) values('" + date + "'," + data[0] + "," + data[1] + "," + data[2] + ")", conn);
+                msc.ExecuteNonQuery();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection conn = new MySqlConnection(Conn))
+            {
+                DataSet ds = new DataSet();
+                string sql = "select * from drive_data";
+                MySqlDataAdapter adpt = new MySqlDataAdapter(sql, conn);
+                adpt.Fill(ds, "drive_data");
+
+                listView1.Items.Clear();
+                chart1.Series[0].Points.Clear();
+                chart1.Series[1].Points.Clear();
+                chart1.Series[2].Points.Clear();
+
+                for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+                {
+                    //한 레코드씩 리스트뷰에 집어넣음
+                    ListViewItem lvi = new ListViewItem();
+                    lvi.Text = ds.Tables[0].Rows[i]["num"].ToString();
+
+                    string date = ds.Tables[0].Rows[i]["date"].ToString();
+                    string collision = ds.Tables[0].Rows[i]["collision"].ToString();
+                    string distance = ds.Tables[0].Rows[i]["distance"].ToString();
+                    string deltha = ds.Tables[0].Rows[i]["deltha"].ToString();
+
+                    lvi.SubItems.Add(date);
+                    lvi.SubItems.Add(collision);
+                    lvi.SubItems.Add(distance);
+                    lvi.SubItems.Add(deltha);
+
+                    listView1.Items.Add(lvi);
+
+                    //한 레코드씩 그래프에 출력
+                    chart1.Series[0].Points.AddXY(i,double.Parse(collision));
+                    chart1.Series[1].Points.AddXY(i, double.Parse(distance));
+                    chart1.Series[2].Points.AddXY(i, double.Parse(deltha));
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if(listView1.Items.Count > 0)
+            {
+                string path = "./output.csv";
+                FileStream fs = new FileStream(path,FileMode.Create);
+                StreamWriter sw = new StreamWriter(fs);
+
+                for(int i=0;i<listView1.Items.Count;i++)
+                {
+                    string line = "";
+                    for(int j=0;j<4;j++)
+                    {
+                        line += listView1.Items[i].SubItems[j].Text+",";
+                    }
+                    line += listView1.Items[i].SubItems[4].Text;
+
+                    sw.WriteLine(line);
+                }
+
+                sw.Close();
+                sw.Dispose();
+                fs.Close();
+
+                fs.Dispose();
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {}
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e) {}
+        private void label2_Click(object sender, EventArgs e) {}
+        private void label3_Click(object sender, EventArgs e) {}
+        private void label2_Click_1(object sender, EventArgs e) {}
+        private void label4_Click(object sender, EventArgs e){}
+        private void label5_Click(object sender, EventArgs e){}
+    }
+}
